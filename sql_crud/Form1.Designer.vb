3f1d893c55a5439c4cd4da6b8ea04085adf785c2
7Louieee <hed-jldalapo@smu.edu.ph>
2025-11-21 18:01:38 +0800

    input of name, age, email & create button

--- sql_crud/Form1.Designer.vb
+++ sql_crud/Form1.Designer.vb
@@ -23,28 +23,108 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.ButtonConnect = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.txtAge = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtEmail = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.btnCreate = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'ButtonConnect
         '
-        Me.ButtonConnect.Location = New System.Drawing.Point(327, 203)
+        Me.ButtonConnect.Location = New System.Drawing.Point(12, 12)
         Me.ButtonConnect.Name = "ButtonConnect"
-        Me.ButtonConnect.Size = New System.Drawing.Size(277, 158)
+        Me.ButtonConnect.Size = New System.Drawing.Size(226, 83)
         Me.ButtonConnect.TabIndex = 0
         Me.ButtonConnect.Text = "Connect My SQL"
         Me.ButtonConnect.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(9, 121)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(41, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Name: "
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(58, 118)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(180, 20)
+        Me.txtName.TabIndex = 2
+        '
+        'txtAge
+        '
+        Me.txtAge.Location = New System.Drawing.Point(58, 144)
+        Me.txtAge.Name = "txtAge"
+        Me.txtAge.Size = New System.Drawing.Size(180, 20)
+        Me.txtAge.TabIndex = 4
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(9, 147)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(32, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Age: "
+        '
+        'txtEmail
+        '
+        Me.txtEmail.Location = New System.Drawing.Point(58, 170)
+        Me.txtEmail.Name = "txtEmail"
+        Me.txtEmail.Size = New System.Drawing.Size(180, 20)
+        Me.txtEmail.TabIndex = 6
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(9, 173)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(35, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Email:"
+        '
+        'btnCreate
+        '
+        Me.btnCreate.Location = New System.Drawing.Point(12, 207)
+        Me.btnCreate.Name = "btnCreate"
+        Me.btnCreate.Size = New System.Drawing.Size(226, 83)
+        Me.btnCreate.TabIndex = 7
+        Me.btnCreate.Text = "CREATE [Insert Data]"
+        Me.btnCreate.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(947, 586)
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(490, 333)
+        Me.Controls.Add(Me.btnCreate)
+        Me.Controls.Add(Me.txtEmail)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtAge)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.ButtonConnect)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents ButtonConnect As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents txtAge As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtEmail As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents btnCreate As Button
 End Class
